82e2689203900800eb48989a7a575dd095adc119
Deepak Joshi <deepu.joshi92@gmail.com>
2021-09-05 16:39:30 +0530

    371. Sum of Two Integers

---
+++ misc/src/L371.java
@@ -0,0 +1,20 @@
+/*
+* 371. Sum of Two Integers
+*
+* Given two integers a and b, return the sum of the two integers without using the operators + and -.
+*
+*
+* */
+public class L371 {
+    public int getSum(int a, int b) {
+        int c =0;
+        while(b != 0) {
+            c = a&b;
+            a = a^b;
+            b = c<<1;
+        }
+
+        return a;
+
+    }
+}
