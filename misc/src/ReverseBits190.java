ffd18ba347c8b04fb42e81d03f9ef5f8a1b1d47a
Deepak Joshi <deepu.joshi92@gmail.com>
2021-09-21 00:22:02 +0530

    Algo Study Plan Day-14

---
+++ misc/src/ReverseBits190.java
@@ -0,0 +1,19 @@
+/*
+*
+* 190. Reverse Bits
+*
+* Reverse bits of a given 32 bits unsigned integer.
+*
+* */
+public class ReverseBits190 {
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result += n & 1;
+            n >>>= 1;   // CATCH: must do unsigned shift
+            if (i < 31) // CATCH: for last digit, don't shift!
+                result <<= 1;
+        }
+        return result;
+    }
+}
