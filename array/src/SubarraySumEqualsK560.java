9a0ffc0a131afbd128afe8405cd500cf246d4973
Deepak Joshi <deepu.joshi92@gmail.com>
2021-09-25 23:55:18 +0530

    DS-II Study Plan day-5

---
+++ array/src/SubarraySumEqualsK560.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+/*
+*
+* 560. Subarray Sum Equals K
+*
+* Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+*
+* */
+public class SubarraySumEqualsK560 {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap < Integer, Integer > map = new HashMap< >();
+        map.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
