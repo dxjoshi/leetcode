ebdca8fff14ec073ec27f9b45e17daa746dc947b
Deepak Joshi <deepu.joshi92@gmail.com>
2021-10-01 23:58:23 +0530

    Sorting Algorithms Practice - Insertion Sort 2

--- sorting/src/InsertionSort.java
+++ sorting/src/InsertionSort.java
@@ -6,15 +6,15 @@ public class InsertionSort implements Sort
     @Override
     public void sort(int[] input) {
         long startTime = System.nanoTime();
-        int idx;
 
         for (int i=0; i<input.length-1; i++) {
-            for (int j = i+1; j>=1; j--) {
+            for (int j = i+1; j>0; j--) {
                 if(input[j] < input[j-1]) {
                     Util.swap(input, j, j-1);
+                } else {
+                    break;
                 }
             }
-
         }
 
         Util.print("Output Array: ", input);
