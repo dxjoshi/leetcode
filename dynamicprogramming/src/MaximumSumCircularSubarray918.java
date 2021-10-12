8ff717b0d6afe5d51cd397316d6a7e0364c2c9c6
Deepak Joshi <deepu.joshi92@gmail.com>
2021-10-12 17:00:56 +0530

    DP-I Study Plan Day-5

---
+++ dynamicprogramming/src/MaximumSumCircularSubarray918.java
@@ -0,0 +1,25 @@
+/*
+*
+* 918. Maximum Sum Circular Subarray
+*
+* Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
+*
+* A circular array means the end of the array connects to the beginning of the array. Formally, the next element of nums[i] is nums[(i + 1) % n] and the previous element of nums[i] is nums[(i - 1 + n) % n].
+*
+* A subarray may only include each element of the fixed buffer nums at most once. Formally, for a subarray nums[i], nums[i + 1], ..., nums[j], there does not exist i <= k1, k2 <= j with k1 % n == k2 % n.
+*
+* */
+public class MaximumSumCircularSubarray918 {
+    // Kadane's algo
+    public int maxSubarraySumCircular(int[] A) {
+        int total = 0, maxSum = A[0], curMax = 0, minSum = A[0], curMin = 0;
+        for (int a : A) {
+            curMax = Math.max(curMax + a, a);
+            maxSum = Math.max(maxSum, curMax);
+            curMin = Math.min(curMin + a, a);
+            minSum = Math.min(minSum, curMin);
+            total += a;
+        }
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+    }
+}
