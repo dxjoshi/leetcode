3d3b2d2e30260213ca80ad7a056ca5b41e01ee40
Deepak Joshi <deepu.joshi92@gmail.com>
2022-01-28 10:20:31 +0530

    FindDuplicate287

---
+++ array/src/FindDuplicate287.java
@@ -0,0 +1,23 @@
+/*
+*
+* 287. Find the Duplicate Number
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+There is only one repeated number in nums, return this repeated number.
+You must solve the problem without modifying the array nums and uses only constant extra space.
+* */
+
+public class FindDuplicate287 {
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        slow = 0;
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
