5c625fef6182f5728a1a9049daf6f776b743e6d6
Deepak Joshi <deepu.joshi92@gmail.com>
2022-01-31 22:41:23 +0530

    SubarrayWithGivenXor

--- algorithms/src/LongestSubstringWithoutRepeatingCharacters3.java
+++ algorithms/src/LongestSubstringWithoutRepeatingCharacters3.java
@@ -22,4 +22,15 @@ public class LongestSubstringWithoutRepeatingCharacters3 {
         }
         return max;
     }
+
+    public int lengthOfLongestSubstring2(String s) {
+        int result = 0;
+        int[] cache = new int[256];
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            j = (cache[s.charAt(i)] > 0) ? Math.max(j, cache[s.charAt(i)]) : j;
+            cache[s.charAt(i)] = i + 1;
+            result = Math.max(result, i - j + 1);
+        }
+        return result;
+    }
 }
