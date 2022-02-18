53c7def5303306f0046e4335584b5f689ed9613c
Deepak Joshi <deepu.joshi92@gmail.com>
2022-02-18 22:28:08 +0530

    MaximumWidthOfBinaryTree662

---
+++ tree/src/MaximumWidthOfBinaryTree662.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+/*
+Given the root of a binary tree, return the maximum width of the given tree.
+
+The maximum width of a tree is the maximum width among all levels.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and rightmost non-null nodes), where the null nodes between the end-nodes are also counted into the length calculation.
+
+It is guaranteed that the answer will in the range of 32-bit signed integer.
+*/
+public class MaximumWidthOfBinaryTree662 {
+    public int widthOfBinaryTree(TreeNode root) {
+        if(root == null) return 0;
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        Map<TreeNode, Integer> m = new HashMap<TreeNode, Integer>();
+        q.offer(root);
+        m.put(root, 1);
+        int curW = 0;
+        int maxW = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            int start = 0;
+            int end = 0;
+            for(int i = 0; i < size; i++){
+                TreeNode node = q.poll();
+                if(i == 0) start = m.get(node);
+                if(i == size - 1) end = m.get(node);
+                if(node.left != null){
+                    m.put(node.left, m.get(node) * 2);
+                    q.offer(node.left);
+                }
+                if(node.right != null){
+                    m.put(node.right, m.get(node) * 2 + 1);
+                    q.offer(node.right);
+                }
+            }
+            curW = end - start + 1;
+            maxW = Math.max(curW, maxW);
+        }
+        return maxW;
+    }
+}
