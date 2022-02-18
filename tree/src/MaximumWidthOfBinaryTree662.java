5e801b7e5f0ea15615194ab9315159782d23ca89
Deepak Joshi <deepu.joshi92@gmail.com>
2022-02-18 22:31:12 +0530

    MaximumWidthOfBinaryTree662

--- tree/src/MaximumWidthOfBinaryTree662.java
+++ tree/src/MaximumWidthOfBinaryTree662.java
@@ -12,7 +12,43 @@ The width of one level is defined as the length between the end-nodes (the leftm
 
 It is guaranteed that the answer will in the range of 32-bit signed integer.
 */
+
+class Pair {
+    TreeNode node;
+    int num;
+    Pair(TreeNode _node, int _num) {
+        num = _num;
+        node = _node;
+    }
+}
+
 public class MaximumWidthOfBinaryTree662 {
+
+    public int widthOfBinaryTreeTwo(TreeNode root) {
+        if(root == null) return 0;
+        int ans = 0;
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(root, 0));
+        while(!q.isEmpty()){
+            int size = q.size();
+            int mmin = q.peek().num;    //to make the id starting from zero
+            int first = 0,last = 0;
+            for(int i=0; i<size; i++){
+                int cur_id = q.peek().num-mmin;
+                TreeNode node = q.peek().node;
+                q.poll();
+                if(i==0) first = cur_id;
+                if(i==size-1) last = cur_id;
+                if(node.left != null)
+                    q.offer(new Pair(node.left, cur_id*2+1));
+                if(node.right != null)
+                    q.offer(new Pair(node.right, cur_id*2+2));
+            }
+            ans = Math.max(ans, last-first+1);
+        }
+        return ans;
+    }
+
     public int widthOfBinaryTree(TreeNode root) {
         if(root == null) return 0;
         Queue<TreeNode> q = new LinkedList<TreeNode>();
