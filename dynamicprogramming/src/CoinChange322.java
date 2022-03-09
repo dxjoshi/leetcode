53c2061b5ac72bbca7031ea08311ef5b9e2d1527
Deepak Joshi <deepu.joshi92@gmail.com>
2022-03-09 19:31:06 +0530

    CoinChange322

--- dynamicprogramming/src/CoinChange322.java
+++ dynamicprogramming/src/CoinChange322.java
@@ -32,4 +32,30 @@ public class CoinChange322 {
             return dp[i+1][amount] = solve(coins, amount, i+1);
         }
     }
+
+    // https://leetcode.com/problems/coin-change/discuss/1274300/Java-DP-Explained-with-comments-Super-Simple
+    // https://leetcode.com/problems/coin-change/discuss/77368/*Java*-Both-iterative-and-recursive-solutions-with-explanations
+
+    public int coinChangeTwo(int[] coins, int amount) {
+        int[][] dp  = new int[coins.length+1][amount+1];
+        for (int i = 0; i<=coins.length; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j<=amount; j++) {
+            dp[0][j] = Integer.MAX_VALUE-1;
+        }
+
+        for (int i = 1; i<=coins.length; i++) {
+            for (int j = 1; j<=amount; j++) {
+                if(coins[i-1] <= j) {
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j- coins[i-1]] + 1) ;
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[coins.length][amount] != Integer.MAX_VALUE-1 ? dp[coins.length][amount]: -1 ;
+
+    }
+
 }
