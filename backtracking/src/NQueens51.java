2f4ca738c9c287b6255488f4f64f48cc8b71984d
Deepak Joshi <deepu.joshi92@gmail.com>
2022-03-10 09:40:07 +0530

    CoinChange322 + NQueens51

---
+++ backtracking/src/NQueens51.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NQueens51 {
+    // Diagonal Set logic in comments: https://leetcode.com/problems/n-queens/discuss/19828/Comparably-concise-Java-code
+    // https://leetcode.com/problems/n-queens/discuss/19804/Share-my-JAVA-DFS-solution-very-easy-to-understand
+
+    List<List<String>> result = new ArrayList<>();
+    public List<List<String>> solveNQueens(int n) {
+        boolean[] visited = new boolean[n];
+
+        boolean[] dia1 = new boolean[2*n-1];
+        boolean[] dia2 = new boolean[2*n-1];
+
+        fun(n, new ArrayList<String>(),visited,dia1,dia2,0);
+
+        return result;
+    }
+
+    private void fun(int n, List<String> list, boolean[] visited, boolean[] dia1, boolean[] dia2, int rowIndex){
+        if(rowIndex == n){
+            result.add(new ArrayList<String>(list));
+            return;
+        }
+
+        for(int i=0;i<n;i++){
+            if(visited[i] || dia1[rowIndex+i] || dia2[rowIndex-i+n-1])
+                continue;
+
+            char[] charArray = new char[n];
+            Arrays.fill(charArray,'.');
+
+            charArray[i] = 'Q';
+            String stringArray = new String(charArray);
+            list.add(stringArray);
+            visited[i] = true;
+            dia1[rowIndex+i] = true;
+            dia2[rowIndex-i+n-1] = true;
+
+            fun(n,list,visited,dia1,dia2,rowIndex+1);
+
+            list.remove(list.size()-1);
+            charArray[i] = '.';
+            visited[i] = false;
+            dia1[rowIndex+i] = false;
+            dia2[rowIndex-i+n-1] = false;
+        }
+    }
+
+}
