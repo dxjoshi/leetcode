48da552c93f973e14f5e381528a058d1cd675372
Deepak Joshi <deepu.joshi92@gmail.com>
2022-03-13 09:10:23 +0530

    AllocateBooks_BinarySearch

---
+++ array/src/AllocateBooks_BinarySearch.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class AllocateBooks_BinarySearch {
+    static boolean isPossible(ArrayList < Integer > A, int pages, int students) {
+        int cnt = 0;
+        int sumAllocated = 0;
+        for (int i = 0; i < A.size(); i++) {
+            if (sumAllocated + A.get(i) > pages) {
+                cnt++;
+                sumAllocated = A.get(i);
+                if (sumAllocated > pages) return false;
+            } else {
+                sumAllocated += A.get(i);
+            }
+        }
+        if (cnt < students) return true;
+        return false;
+    }
+
+    public static int books(ArrayList< Integer > A, int B) {
+        if (B > A.size()) return -1;
+        int low = A.get(0);
+        int high = 0;
+        for (int i = 0; i < A.size(); i++) {
+            high = high + A.get(i);
+            low = Math.min(low, A.get(i));
+        }
+        while (low <= high) {
+            int mid = (low + high) >> 1;
+
+            if (isPossible(A, mid, B)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        // return res -> this is also correct
+        return low;
+    }
+}
