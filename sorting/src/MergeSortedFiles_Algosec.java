b6c34fc6ed3bad43250939cd50c5a3f79d254d98
Deepak Joshi <deepu.joshi92@gmail.com>
2022-03-29 20:20:37 +0530

    Sort a very large file with a small heap size

---
+++ sorting/src/MergeSortedFiles_Algosec.java
@@ -0,0 +1,91 @@
+import java.io.*;
+import java.util.*;
+
+public class MergeSortedFiles_Algosec {
+
+    public static void main(String[] args) {
+        List<File> inputFiles = Arrays.asList(new File("C:\\code\\leetcode\\sorting\\src\\resources\\p1.txt"),
+                new File("C:\\code\\leetcode\\sorting\\src\\resources\\p2.txt"),
+                new File("C:\\code\\leetcode\\sorting\\src\\resources\\p3.txt"));
+        File outputFile = new File("C:\\code\\leetcode\\sorting\\src\\resources\\output.txt");
+        solve( inputFiles, outputFile);
+    }
+
+    static class Pair {
+        String line;
+        Integer fileId;
+
+        public Pair(String line, Integer fileId) {
+            this.line = line;
+            this.fileId = fileId;
+        }
+    }
+
+    public static void solve(List<File> sortedFiles, File outputFile) {
+        // files sorted
+        // until file has reached end check all forthe smallest and add it to output
+        PriorityQueue<Pair> pq  = new PriorityQueue<>(sortedFiles.size(), Comparator.comparing(a -> a.line));
+        List<BufferedReader> readers = new ArrayList<>();
+        BufferedWriter writer = null;
+        try {
+
+            for (int i=0; i < sortedFiles.size(); i++) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(sortedFiles.get(i))));
+                readers.add(reader);
+            }
+
+            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile)));
+
+            for (int i=0; i < readers.size(); i++) {
+                addToQueue(readers, i, pq);
+            }
+
+            while (!pq.isEmpty()){
+                Pair pair = pq.poll();
+                String line = pair.line;
+                Integer fileId = pair.fileId;
+                writer.write(line);
+                writer.newLine();
+                System.out.println(line);
+                addToQueue(readers, fileId, pq);
+            }
+            writer.close();
+            close(readers);
+        }catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+        }
+    }
+
+    private static void close(List<BufferedReader> readers) {
+        readers.stream().forEach(bufferedReader -> {
+            try {
+                bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+    private static void addToQueue(List<BufferedReader> readers, int fileId, PriorityQueue<Pair> pq) throws IOException {
+        BufferedReader reader = readers.get(fileId);
+        String line = reader.readLine();
+        if (line != null && !line.isEmpty()) {
+            pq.offer(new Pair(line, fileId));
+        }
+    }
+
+
+    //
+
+    //            0        1
+    // 0 p1 -> Eugene, Prakash,
+    // 1 p2 -> Deepak, Ram, Shiva, Tarun ,Varun
+    // 2 p3 -> Vohit
+
+    // Output ->  Deepak, Eugene, PRakash,
+
+
+
+    //    File -> List<String> -> 1 GB -> Sort -> 200 MB
+
+}
