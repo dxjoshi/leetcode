2ef59b1068358d3ffb1bf72f0e542d1e1ca06311
Deepak Joshi <deepu.joshi92@gmail.com>
2022-04-04 21:25:55 +0530

    Custom DB connection pool

---
+++ custom-implementation/src/customConnectionPool/BasicConnectionPool.java
@@ -0,0 +1,98 @@
+package customConnectionPool;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BasicConnectionPool implements IConnectionPool {
+
+    private String url;
+    private String user;
+    private String password;
+    private List<Connection> connectionPool;
+    private List<Connection> usedConnections = new ArrayList<>();
+    private static int INITIAL_POOL_SIZE = 10;
+    private static int MAX_POOL_SIZE = 20;
+    private static int MAX_TIMEOUT = 60;
+
+    private BasicConnectionPool(String url, String user, String password, List<Connection> connectionPool) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+        this.connectionPool = connectionPool;
+    }
+
+    public static BasicConnectionPool create(String url, String user, String password) throws SQLException {
+        List<Connection> pool = new ArrayList<>(INITIAL_POOL_SIZE);
+        for (int i = 0; i < INITIAL_POOL_SIZE; i++) {
+            pool.add(createConnection(url, user, password));
+        }
+        return new BasicConnectionPool(url, user, password, pool);
+    }
+
+    @Override
+    public Connection getConnection() {
+        if (connectionPool.isEmpty()) {
+            if (usedConnections.size() < MAX_POOL_SIZE) {
+                try {
+                    connectionPool.add(createConnection(url, user, password));
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } else throw new RuntimeException("Maximum pool size reached!");
+        }
+
+        Connection connection = connectionPool
+                .remove(connectionPool.size() - 1);
+
+        try {
+            if(!connection.isValid(MAX_TIMEOUT)){
+                connection = createConnection(url, user, password);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        usedConnections.add(connection);
+        return connection;
+    }
+
+    @Override
+    public boolean releaseConnection(Connection connection) {
+        connectionPool.add(connection);
+        return usedConnections.remove(connection);
+    }
+
+    @Override
+    public String getUrl() {
+        return null;
+    }
+
+    @Override
+    public String getUser() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    private static Connection createConnection(String url, String user, String password) throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public int getSize() {
+        return connectionPool.size() + usedConnections.size();
+    }
+
+    public void shutdown() throws SQLException {
+        usedConnections.forEach(this::releaseConnection);
+        for (Connection c : connectionPool) {
+            c.close();
+        }
+        connectionPool.clear();
+    }
+}
